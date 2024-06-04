560adb72ac4c891a96a40cb21279df16ac9c25d9
pr1amos <157016419+pr1amos@users.noreply.github.com>
2024-06-04 15:54:51 +0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+
+while my_list[index] >= 0:
+    if index >= len(my_list):
+        break
+    print(my_list[index])
+    index += 1
